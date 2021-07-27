6084c38c627fa19374d93d8de8fda19db2e00132
Arecio Canton <cuba289@gmail.com>
2021-07-27 11:30:13 -0400

    working on header

---
+++ src/components/header/Header.js
@@ -0,0 +1,20 @@
+import './header.css';
+
+function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="row justify-content-between">
+                    <div className="logoContainer">
+                        <a className="logo" href="/">A</a>
+                    </div>
+                    <div className="burgerBtn">
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
