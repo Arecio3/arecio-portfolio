04e8f129fe68b67e77871104e2993b1b4e3ca6df
Arecio Canton <cuba289@gmail.com>
2021-08-08 03:54:55 -0400

    nav menu active issue is going to ice box

--- src/components/navMenu/NavMenu.js
+++ src/components/navMenu/NavMenu.js
@@ -11,11 +11,11 @@ function NavMenu({ menuOpen, setMenuOpen, theme, setTheme }) {
             <div className={"nav-menu-inner"}>
                 <ul>
                    <Link to='/'><li onClick={() => setMenuOpen(false)} className={theme === "dark" ? "outer-shadow-dm hover-in-shadow-dm active" : "inner-shadow active"}>Home</li></Link>
-                   <Link to='/about'><li onClick={() => setMenuOpen(false)} className={theme === "dark" ? "inner-shadow-dm" : "outer-shadow hover-in-shadow"}>About</li></Link>
-                   <Link to='/services'><li onClick={() => setMenuOpen(false)}className={theme === "dark" ? "inner-shadow-dm" : "outer-shadow hover-in-shadow"}>Services</li></Link>
-                   <Link to='/portfolio'><li onClick={() => setMenuOpen(false)}className={theme === "dark" ? "inner-shadow-dm" : "outer-shadow hover-in-shadow"}>Portfolio</li></Link>
-                    <Link to='/testimonials'><li onClick={() => setMenuOpen(false)}className={theme === "dark" ? "inner-shadow-dm" : "outer-shadow hover-in-shadow"}>Testimonials</li></Link>
-                   <Link to='/contact'><li  onClick={() => setMenuOpen(false)} className={theme === "dark" ? "inner-shadow-dm" : "outer-shadow hover-in-shadow"}>Contact</li></Link>
+                   <Link to='/about'><li onClick={() => setMenuOpen(false)} className={theme === "dark" ? "inner-shadow-dm active" : "outer-shadow hover-in-shadow active"}>About</li></Link>
+                   <Link to='/services'><li onClick={() => setMenuOpen(false)}className={theme === "dark" ? "inner-shadow-dm active" : "outer-shadow hover-in-shadow active"}>Services</li></Link>
+                   <Link to='/portfolio'><li onClick={() => setMenuOpen(false)}className={theme === "dark" ? "inner-shadow-dm active" : "outer-shadow hover-in-shadow active"}>Portfolio</li></Link>
+                    <Link to='/testimonials'><li onClick={() => setMenuOpen(false)}className={theme === "dark" ? "inner-shadow-dm active" : "outer-shadow hover-in-shadow active"}>Testimonials</li></Link>
+                   <Link to='/contact'><li  onClick={() => setMenuOpen(false)} className={theme === "dark" ? "inner-shadow-dm active" : "outer-shadow hover-in-shadow active"}>Contact</li></Link>
                 </ul>
             </div>
             {/* Copyright */}
